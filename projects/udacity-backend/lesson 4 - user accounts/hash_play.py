aed9078c768625b088292047b1374dc15bf79a4e
Cody Williams <codywill77@gmail.com>
2017-04-08 12:55:55 -0700

    Added salt to hash

--- projects/udacity-backend/lesson 4 - user accounts/hash_play.py
+++ projects/udacity-backend/lesson 4 - user accounts/hash_play.py
@@ -1,5 +1,7 @@
 import hashlib
 import hmac
+import random
+import string
 
 #def hash_str(s):
 #    return hashlib.md5(s).hexdigest()
@@ -27,3 +29,28 @@ def check_secure_val(h):
 SECRET = 'imsosecret'
 def hash_str(s):
     return hmac.new(SECRET,s).hexdigest()
+    
+# implement the function make_salt() that returns a string of 5 random
+# letters use python's random module.
+# Note: The string package might be useful here.
+
+def make_salt():
+    return ''.join(random.choice(string.letters) for x in xrange(5))
+
+# implement the function make_pw_hash(name, pw) that returns a hashed password 
+# of the format: 
+# HASH(name + pw + salt),salt
+# use sha256
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()
+    return "%s,%s" % (hashlib.sha256(name + pw + salt).hexdigest(), salt)
+
+# Implement the function valid_pw() that returns True if a user's password 
+# matches its hash. You will need to modify make_pw_hash.
+
+def valid_pw(name, pw, h):
+    salt = h.split(',')[1]
+    return h == make_pw_hash(name, pw, salt)
+
